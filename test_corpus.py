62d63086a21672cc860fb948ae12905a75e9b7f6
JacksonLLee <jsllee.phon@gmail.com>
2014-12-14 16:35:00 -0600

    character search added

--- test_corpus.py
+++ test_corpus.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# coding: utf-8
+
 import pycantonese as pc # assuming pycantonese.py is in the same directory
 
 ##----------------------------------------------------------------------------##
@@ -15,38 +18,38 @@ def printListUTF8(stringlist):
         try:
             print unicode(word, encoding='utf-8')
         except:
-            pass
+            print repr(word)
 
 def final_tone(corpus, what_final_tone):
     final = what_final_tone[: -1]
     tone = what_final_tone[-1]
-    return pc.search(corpus, [(final, 2), (tone, 3)], 'set')
+    return pc.search_jp(corpus, [(final, 2), (tone, 3)], 'type')
 
 ##----------------------------------------------------------------------------##
 ## sample usage of PyCantonese
 
 print '\nFind all words with a specific tone (e.g., tone 4):'
-tone4 = pc.tone(currentCorpus, '4', 'set')
+tone4 = pc.tone(currentCorpus, '4', 'type')
 print 'There are %d matching words, e.g.:' % (len(tone4))
 printListUTF8(tone4[: 10]) # 10 results
 
 print '\nFind all words with a specific onset/initial (e.g., \'b\'):'
-b = pc.onset(currentCorpus, 'b', 'set') # the function initial() is the same
+b = pc.onset(currentCorpus, 'b', 'type') # the function initial() is the same
 print 'There are %d matching words, e.g.:' % (len(b))
 printListUTF8(b[: 10]) # 10 results
 
 print '\nFind all words with a specific nucleus (e.g., \'aa\'):'
-aa = pc.nucleus(currentCorpus, 'aa', 'set')
+aa = pc.nucleus(currentCorpus, 'aa', 'type')
 print 'There are %d matching words, e.g.:' % (len(aa))
 printListUTF8(aa[: 10]) # 10 results
 
 print '\nFind all words with a specific coda (e.g., \'ng\'):'
-ng = pc.coda(currentCorpus, 'ng', 'set')
+ng = pc.coda(currentCorpus, 'ng', 'type')
 print 'There are %d matching words, e.g.:' % (len(ng))
 printListUTF8(ng[: 10]) # 10 results
 
 print '\nFind all words with a specific final (e.g., \'aan\'):'
-aan = pc.final(currentCorpus, 'aan', 'set')
+aan = pc.final(currentCorpus, 'aan', 'type')
 print 'There are %d matching words, e.g.:' % (len(aan))
 printListUTF8(aan[: 10]) # 10 results
 
@@ -55,4 +58,9 @@ print '\n*** using a customized function not from PyCantonese ***\n' + \
 t3 = final_tone(currentCorpus, 't3')
 print 'There are %d matching words, e.g.:' % (len(t3))
 printListUTF8(t3[: 10]) # 10 results
+
+print '\nFind all words with a specific character (e.g., \'我\'):'
+ngo5 = pc.character(currentCorpus, '我', 'type')
+print 'There are %d matching words, e.g.:' % (len(ngo5))
+printListUTF8(ngo5[: 10]) # 10 results
 
