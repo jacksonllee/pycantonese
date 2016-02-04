57a199b283b20f3ec712c3dbf8695c69edd8344f
JacksonLLee <jsllee.phon@gmail.com>
2016-02-03 23:08:46 -0600

    add VERSION_DEV

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
+from os import path
 import sys
 from setuptools import (setup, find_packages)
 
@@ -10,59 +10,58 @@ if py_version < (3, 4):
     sys.exit('Error: PyCantonese requires Python 3.4 or above.\n'
              'You are using Python {}.{}.'.format(*py_version))
 
-version_filename = os.path.join(os.path.dirname(__file__),
-                                'pycantonese', 'VERSION')
-with open(version_filename) as f:
+if __name__ == '__main__':
+    version_fname = 'VERSION_DEV'
+else:
+    version_fname = 'VERSION'
+
+version_path = path.join(path.dirname(__file__), 'pycantonese', version_fname)
+with open(version_path) as f:
     package_version = f.read().strip()
 
-setup(name="pycantonese",
-    version=package_version,
-    description="PyCantonese",
-
-    long_description="""
-PyCantonese is a Python library for working with Cantonese corpus data. While it is under active development and many other features and functions are forthcoming, it currently includes JyutPing parsing and conversion tools as well as general search functionalities for built-in or custom corpus data.
-
-Documentation: `http://pycantonese.org/ <http://pycantonese.org/>`_
-""",
-
-    url="http://pycantonese.org/",
-    author="Jackson Lee",
-    author_email="jsllee.phon@gmail.com",
-    license="Apache License, Version 2.0",
-    packages=find_packages(),
-    keywords=['computational linguistics', 'natural language processing',
+setup(name='pycantonese',
+      version=package_version,
+      description='PyCantonese',
+      long_description='PyCantonese: Cantonese Linguistics in Python',
+      url='http://pycantonese.org/',
+      author='Jackson Lee',
+      author_email='jsllee.phon@gmail.com',
+      license='Apache License, Version 2.0',
+      packages=find_packages(),
+      keywords=['computational linguistics', 'natural language processing',
                 'NLP', 'Cantonese', 'linguistics', 'corpora', 'speech',
                 'language', 'Chinese', 'Jyutping', 'NLTK', 'tagging'],
-    install_requires=["nltk"],
 
-    package_data={
-        "pycantonese": ["data/hkcancor/*"],
-    },
+      install_requires=['nltk', 'pylangacq'],
 
-    zip_safe=False,
+      package_data={
+          'pycantonese': ['data/hkcancor/*', 'VERSION', 'VERSION_DEV'],
+      },
 
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: Chinese (Traditional)',
-        'Natural Language :: Cantonese',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'Topic :: Scientific/Engineering :: Human Machine Interfaces',
-        'Topic :: Scientific/Engineering :: Information Analysis',
-        'Topic :: Text Processing',
-        'Topic :: Text Processing :: Filters',
-        'Topic :: Text Processing :: General',
-        'Topic :: Text Processing :: Indexing',
-        'Topic :: Text Processing :: Linguistic'
-    ],
-)
+      zip_safe=False,
 
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Information Technology',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: Apache Software License',
+          'Natural Language :: Chinese (Traditional)',
+          'Natural Language :: Cantonese',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence',
+          'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+          'Topic :: Scientific/Engineering :: Information Analysis',
+          'Topic :: Text Processing',
+          'Topic :: Text Processing :: Filters',
+          'Topic :: Text Processing :: General',
+          'Topic :: Text Processing :: Indexing',
+          'Topic :: Text Processing :: Linguistic',
+          ],
+      )
