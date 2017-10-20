4dfb5ee688aa8bdd725e7fb97331f36c8bb47572
jacksonlee <jacksonlunlee@gmail.com>
2017-10-20 15:13:18 -0500

    TST Test parse_jyutping for syllabic nasals

--- pycantonese/tests/test_jyutping.py
+++ pycantonese/tests/test_jyutping.py
@@ -6,3 +6,10 @@ from pycantonese.jyutping import parse_jyutping
 def test_parse_jyutping_wrong_data_type():
     with pytest.raises(ValueError):
         parse_jyutping(123)
+
+
+def test_parse_jyutping_syllabic_nasals():
+    # TODO assert parse_jyutping('hm4') == [('h', 'm', '', '4')]
+    assert parse_jyutping('ng5') == [('', 'ng', '', '5')]
+    assert parse_jyutping('m4') == [('', 'm', '', '4')]
+    assert parse_jyutping('n3') == [('', 'n', '', '3')]
