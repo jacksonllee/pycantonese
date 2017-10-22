1ee0703a77026cee566068795a3c9cfe0e19a313
jacksonlee <jacksonlunlee@gmail.com>
2017-10-21 22:34:50 -0500

    TST Add test_tipa.py

---
+++ pycantonese/tests/test_tipa.py
@@ -0,0 +1,14 @@
+from pycantonese.jyutping.parse_jyutping import ONSETS, TONES
+from pycantonese.jyutping.tipa import ONSETS_TIPA, TONES_TIPA
+
+
+# TODO def test_correct_nucleus_set():
+# TODO def test_correct_coda_set():
+
+
+def test_correct_onset_set():
+    assert ONSETS_TIPA.keys() == ONSETS
+
+
+def test_correct_tone_set():
+    assert TONES_TIPA.keys() == TONES
