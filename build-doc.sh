dfbf8e90a1067be4b128fa0fb5920598642aa882
jacksonlee <jacksonlunlee@gmail.com>
2017-10-25 09:45:25 -0500

    DOC Version script for adding social media links in docs

--- build-doc.sh
+++ build-doc.sh
@@ -6,7 +6,7 @@ SOURCEPATH=docs/source
 rm $BUILDPATH/*.html
 sphinx-build -b html $SOURCEPATH $BUILDPATH
 
-python insert_social_media_links.py
+python $SOURCEPATH/insert_social_media_links.py
 echo "social media links inserted"
 
 echo 'Documentation website in '$BUILDPATH
