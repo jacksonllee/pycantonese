7275afea9a54e32e39ae98f67ec573667e9c6b4f
jacksonllee <jacksonlunlee@gmail.com>
2017-11-02 21:30:25 -0500

    TST Basic coverage for jyutping2yale

--- pycantonese/tests/test_yale.py
+++ pycantonese/tests/test_yale.py
@@ -1,5 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
 from pycantonese.jyutping.parse_jyutping import ONSETS, NUCLEI, CODAS
-from pycantonese.jyutping.yale import ONSETS_YALE, NUCLEI_YALE, CODAS_YALE
+from pycantonese.jyutping.yale import (
+    ONSETS_YALE, NUCLEI_YALE, CODAS_YALE, jyutping2yale)
 
 
 def test_correct_onset_set():
@@ -12,3 +17,11 @@ def test_correct_nucleus_set():
 
 def test_correct_coda_set():
     assert set(CODAS_YALE.keys()) == CODAS
+
+
+def test_jyutping2yale_m4goi1():
+    assert jyutping2yale('m4goi1') == 'm̀hgōi'
+
+
+def test_jyutping2yale_gwong2dung1waa2():
+    assert jyutping2yale('gwong2dung1waa2') == 'gwóngdūngwá'
