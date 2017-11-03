76fc87db1c3095b5a5c08f5166af9b419a60fc6b
jacksonllee <jacksonlunlee@gmail.com>
2017-11-02 21:32:49 -0500

    TST Basic coverage for jyutping2tipa

--- pycantonese/tests/test_tipa.py
+++ pycantonese/tests/test_tipa.py
@@ -1,5 +1,5 @@
 from pycantonese.jyutping.parse_jyutping import ONSETS, TONES
-from pycantonese.jyutping.tipa import ONSETS_TIPA, TONES_TIPA
+from pycantonese.jyutping.tipa import ONSETS_TIPA, TONES_TIPA, jyutping2tipa
 
 
 # TODO def test_correct_nucleus_set():
@@ -12,3 +12,9 @@ def test_correct_onset_set():
 
 def test_correct_tone_set():
     assert set(TONES_TIPA.keys()) == TONES
+
+
+def test_jyutping2tipa():
+    assert jyutping2tipa('m4goi1') == ['\\s{m}21', 'kOY55']
+    assert jyutping2tipa('gwong2dung1waa2') == [
+        'k\\super w ON25', 'tUN55', 'wa25']
